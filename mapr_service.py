99c9f8955f7966fe554d9dbd34cf9ae97b9d0813
Davinder Pal <dpsangwal@gmail.com>
2018-08-16 20:51:47 +0530

    added initial mapr_service lib

---
+++ mapr_service.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 Davinder Pal <dpsangwal@gmail.com>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1', 'status': ['preview'],
+                    'supported_by': 'individual'}
+
+DOCUMENTATION = \
+    '''
+---
+module: mapr_service
+version_added: "0.1"
+author: "Davinder Pal (@116davinder)"
+short_description: Manage MapR Services by rest api.
+description:
+   - Manage MapR Services
+        (https://mapr.com/docs/52/ReferenceGuide/REST-API-Syntax.html)
+options:
+  app_name:
+    description:
+      - (one of app_name or application_id are required)
+        The value of app_name in the newrelic.yml file used by the application
+    required: false
+  application_id:
+    description:
+      - (one of app_name or application_id are required)
+        (see https://rpm.newrelic.com/api/explore/applications/list)
+    required: false
+  validate_certs:
+    description:
+      - If C(no), SSL certificates will not be validated.
+        This should only be used for personal self-signed certificates.
+    required: false
+    default: 'yes'
+    type: bool
+    version_added: 2.6.x
+'''
+
+EXAMPLES = \
+    '''
+- newrelic_deployment:
+    token: XXXXXXXXX
+    app_name: ansible_app
+    user: ansible_deployment_user
+    revision: '1.X'
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import fetch_url
+import json
+
+
+# ===========================================
+# Module execution.
+#
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec=dict(
+            username=dict(required=True),
+            password=dict(required=True, no_log=False),
+            service_name=dict(required=True),
+            mcs_url=dict(required=True),
+            state=dict(required=True),
+            # host=dict(required=False),
+            validate_certs=dict(default='False', type='bool'),
+        )
+    )
+
+    mapr_default_services = ['apiserver','cldb','fileserver','nfs']
+    mapr_default_service_state = ['start','stop','restart']
+
+    def get_current_hostname():
+        cmd = module.get_bin_path('hostname', True)
+        rc, out, err = module.run_command(cmd)
+        if rc != 0:
+            module.fail_json(msg="Command failed rc=%d, out=%s, err=%s" % (rc, out, err))
+        return to_native(out).strip()
+
+    if (module.params['username'] or module.params['password']
+            or module.params['service_name'] or module.params['mcs_url']
+            or module.params['state'] ):
+        module.fail_json(msg="all values should to be defined")
+    elif module.params['state'] not in mapr_default_service_state:
+        module.fail_json(msg="state should be start/stop/restart only")
+    else:
+        host = get_current_hostname()
+        if module.params['service_name'] in mapr_default_services:
+            url_parameters = ""
+        else:
+            url_parameters = "?action=" + module.params['state'] + "&nodes=" + host + "&name=" + module.params['service_name']
+
+#https://mapr.local:8443/rest/node/services?action=start&nodes=mapr.local&name=grafana
+
+
+    if module.params['app_name']:
+        data = 'filter[name]=' + str(module.params['app_name'])
+        newrelic_api = 'https://api.newrelic.com/v2/applications.json'
+        headers = "'Content-Type': 'application/json'"
+        (resp, info) = fetch_url(module,
+                                 newrelic_api,
+                                 headers=headers,
+                                 data=data,
+                                 method='GET')
+        if info['status'] != 200:
+            module.fail_json(msg="unable to get application list from\
+            newrelic: %s" % info['msg'])
+        else:
+            body = json.loads(resp.read())
+        if body is None:
+            module.fail_json(msg='No Data for applications')
+        else:
+            app_id = body['applications'][0]['id']
+            if app_id is None:
+                module.fail_json(msg="App not found in\
+                NewRelic Registerd Applications List")
+    else:
+        app_id = module.params['application_id']
+
+if __name__ == '__main__':
+    main()
