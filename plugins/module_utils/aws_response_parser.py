5231c322c2803cc345c1880d6b3ee8da44a2054d
Davinder Pal <dpsangwal@gmail.com>
2021-01-08 11:36:31 +0200

    added module util function

---
+++ plugins/module_utils/aws_response_parser.py
@@ -0,0 +1,13 @@
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
+
+
+def aws_response_list_parser(paginate: bool, iterator, resource_field: str) -> list:
+    _return = []
+    if paginate:
+        for response in iterator:
+            for _app in response[resource_field]:
+                _return.append(camel_dict_to_snake_dict(_app))
+    else:
+        for _app in iterator[resource_field]:
+            _return.append(camel_dict_to_snake_dict(_app))
+    return _return
