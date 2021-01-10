a9086ace7768dbab34bed78278e4cbbb0548ea61
Davinder Pal <dpsangwal@gmail.com>
2021-01-11 01:26:08 +0200

    WIP - added new info module for code guru codeguruprofiler

---
+++ plugins/module_utils/utils.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+
+def convert_str_to_datetime(time: str):
+    """
+    convert string time to datetime object.
+    :param time: example "2021-12-01"
+    :return:
+    """
+    try:
+        return datetime.strptime(time, '%Y-%m-%d')
+    except:
+        return None
